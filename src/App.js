96f860a8e205307166d6d53f23c7d3189ae1f9c1
diegonicita <diegonicita@gmail.com>
2022-03-18 14:34:18 -0300

    creando textarea y hooks

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import {useState, useEffect} from "react";
 
 function App() {
+
+const [filiatorios, setFiliatorios] = useState(false);
+
+// useEffect(
+// () => {
+  
+
+// }, [filiatorios]);
+
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <textarea onChange={e=>setFiliatorios(e.target.value)} value={filiatorios}></textarea>
+        <p> Datos Filiatorios {filiatorios} </p>
       </header>
+
+      
     </div>
   );
 }
